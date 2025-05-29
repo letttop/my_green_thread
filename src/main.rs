f3d266d793270cdae828a0f35002b0e091759529
lett <letterr2022@gmail.com>
2025-05-30 00:33:20 +0800

    green thread

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,231 @@
-use core::arch::asm;
+#![feature(naked_functions)]
+use std::arch::{asm, naked_asm};
+const DEFAULT_STACK_SIZE: usize = 1024 * 1024 * 2;
+const MAX_THREADS: usize = 4;
+static mut RUNTIME: usize = 0;
 
-// stack size
-const SSIZE: isize = 48;
+/// state of thread
+#[derive(PartialEq, Eq, Debug)]
+enum State {
+    Available, // free for new task
+    Running,   // running
+    Ready,     // ready for resume
+}
 
-// only one for now
-// but need all callee to save later
 #[derive(Debug, Default)]
 #[repr(C)]
 struct ThreadContext {
+    ra: u64,
     sp: u64,
+    s0: u64, // fp
+    s1: u64,
+    s2: u64,
+    s3: u64,
+    s4: u64,
+    s5: u64,
+    s6: u64,
+    s7: u64,
+    s8: u64,
+    s9: u64,
+    s10: u64,
+    s11: u64,
+    entry: u64,
 }
 
-fn hello() -> ! {
-    println!("I LOVE WAKING UP ON A NEW STACK!");
-    loop {}
+#[unsafe(naked)]
+#[unsafe(no_mangle)]
+unsafe extern "C" fn switch(old: *mut ThreadContext, new: *const ThreadContext) {
+    naked_asm!(
+        "
+        sd ra, 0*8(a0)
+        sd sp, 1*8(a0)
+        sd s0, 2*8(a0)
+        sd s1, 3*8(a0)
+        sd s2, 4*8(a0)
+        sd s3, 5*8(a0)
+        sd s4, 6*8(a0)
+        sd s5, 7*8(a0)
+        sd s6, 8*8(a0)
+        sd s7, 9*8(a0)
+        sd s8, 10*8(a0)
+        sd s9, 11*8(a0)
+        sd s10, 12*8(a0)
+        sd s11, 13*8(a0)
+        sd ra, 14*8(a0)
+
+        ld ra, 0*8(a1)
+        ld sp, 1*8(a1)
+        ld s0, 2*8(a1)
+        ld s1, 3*8(a1)
+        ld s2, 4*8(a1)
+        ld s3, 5*8(a1)
+        ld s4, 6*8(a1)
+        ld s5, 7*8(a1)
+        ld s6, 8*8(a1)
+        ld s7, 9*8(a1)
+        ld s8, 10*8(a1)
+        ld s9, 11*8(a1)
+        ld s10, 12*8(a1)
+        ld s11, 13*8(a1)
+        ld t0, 14*8(a1)
+
+        jr t0
+    "
+    );
 }
 
-fn gt_switch(new: *const ThreadContext) {
-    unsafe {
-        asm!(
-            "ld sp, 0({0})",// {0} 指向第一个输入操作数，也就是reg
-            "ld ra, 0(sp)",
-            "ret",  // jalr rd, rs1, imm ：保存pc+4到rd，跳转imm(rs1); ret = jalr x0, x1, 0, 就是直接跳转x1，x1=ra，
-            in(reg) new,    // 这里的reg由asm!决定，任意临时寄存器
-            options(noreturn)
-        );
+struct Thread {
+    stack: Vec<u8>,
+    ctx: ThreadContext,
+    state: State,
+}
+impl Thread {
+    fn new() -> Self {
+        Thread {
+            stack: vec![0_u8; DEFAULT_STACK_SIZE],
+            ctx: ThreadContext::default(),
+            state: State::Available,
+        }
     }
+}
+
+pub struct Runtime {
+    threads: Vec<Thread>,
+    current: usize,
+}
+
+impl Runtime {
+    /// 0: 正在运行的主线程
+    /// 其他：available的线程
+    pub fn new() -> Self {
+        let base_thread = Thread {
+            stack: vec![0_u8; DEFAULT_STACK_SIZE],
+            ctx: ThreadContext::default(),
+            state: State::Running,
+        };
+        let mut threads = vec![base_thread];
+        let mut available_threads: Vec<Thread> = (1..MAX_THREADS).map(|_| Thread::new()).collect();
+        threads.append(&mut available_threads);
+        Runtime {
+            threads,
+            current: 0,
+        }
+    }
+
+    pub fn init(&self) {
+        unsafe {
+            let r_ptr: *const Runtime = self;
+            RUNTIME = r_ptr as usize;
+        }
+    }
+
+    // 主线程
+    // 不断调用 t_yield()
+    // schedule a new thread to be run
+    // 直到返回 false
+    pub fn run(&mut self) -> ! {
+        while self.t_yield() {}
+        std::process::exit(0);
+    }
+
+    // 非主线程
+    // 完成时调用，设置线程状态
+    // 然后schedule a new thread to be run
+    fn t_return(&mut self) {
+        if self.current != 0 {
+            self.threads[self.current].state = State::Available;
+            self.t_yield();
+        }
+    }
+
+    #[inline(never)]
+    fn t_yield(&mut self) -> bool {
+        // 找到一个ready的thread
+        let mut pos = self.current;
+        while self.threads[pos].state != State::Ready {
+            pos += 1;
+            if pos == self.threads.len() {
+                pos = 0;
+            }
+            if pos == self.current {
+                return false;
+            }
+        }
+        // 如果yield线程不是Available状态调用yield
+        // 那他就是从Running状态调用
+        // 那就需要暂停它
+        if self.threads[self.current].state != State::Available {
+            self.threads[self.current].state = State::Ready;
+        }
+        self.threads[pos].state = State::Running;
+
+        let old_pos = self.current;
+        self.current = pos;
+
+        unsafe {
+            let old: *mut ThreadContext = &mut self.threads[old_pos].ctx;
+            let new: *const ThreadContext = &self.threads[pos].ctx;
+            switch(old, new);
+        }
+
+        // avoid compile optimizing
+        self.threads.len() > 0
+    }
+
+    pub fn spawn(&mut self, f: fn()) {
+        let available = self
+            .threads
+            .iter_mut()
+            .find(|t| t.state == State::Available)
+            .expect("no available thread.");
+
+        let size = available.stack.len();
+        unsafe {
+            let s_ptr = available.stack.as_mut_ptr().offset(size as isize);
+            let s_ptr = (s_ptr as usize & !7) as *mut u8;
+
+            available.ctx.ra = task_return as u64; // task return address
+            available.ctx.sp = s_ptr as u64; // stack pointer
+            available.ctx.entry = f as u64; // task entry address
+        }
+        available.state = State::Ready;
+    }
+}
+
+fn task_return() {
+    unsafe {
+        let rt_ptr = RUNTIME as *mut Runtime;
+        (*rt_ptr).t_return();
+    }
+}
+
+pub fn yield_thread() {
+    unsafe {
+        let rt_ptr = RUNTIME as *mut Runtime;
+        (*rt_ptr).t_yield();
+    };
 }
 
 fn main() {
-    let mut ctx = ThreadContext::default();
-    let mut stack = vec![0_u8; SSIZE as usize];
-    unsafe {
-        // 栈底，就是最高位
-        let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
-        // !15=111...1110000
-        let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
-        // casting function pointer `hello` to `u64`并写入栈内
-        // 此时hello在.text段
-        std::ptr::write(sb_aligned.offset(-8) as *mut u64, hello as usize as u64);
-        // 向下回到栈顶（仅保存，在gt_switch中跳转）
-        ctx.sp = sb_aligned.offset(-8) as u64;
-        // 当调用函数时，会
-        //  保存caller寄存器
-        //  设置a0-a7作为B的输入参数
-        //  设置ra为A的PC+4——————————此时pc指向gt_switch
-        //  分配B的栈空间
-        //  保存B要返回的地址ra
-        //  保存callee寄存器
-        //  运行
-        gt_switch(&ctx);
-    }
+    let mut runtime = Runtime::new();
+    runtime.init();
+    runtime.spawn(|| {
+        println!("THREAD 1 STARTING");
+        let id = 1;
+        for i in 0..10 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 1 FINISHED");
+    });
+    runtime.spawn(|| {
+        println!("THREAD 2 STARTING");
+        let id = 2;
+        for i in 0..15 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 2 FINISHED");
+    });
+    runtime.run();
 }
